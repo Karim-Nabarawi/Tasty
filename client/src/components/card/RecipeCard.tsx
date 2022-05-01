00f1888543288189f39c8e68f86549a214d7d29f
Karim-Nabarawi <K.nabarawi@gmail.com>
2022-05-01 03:40:03 +0200

    updated delete function

--- client/src/components/card/RecipeCard.tsx
+++ client/src/components/card/RecipeCard.tsx
@@ -1,56 +1,44 @@
 import { useNavigate } from "react-router";
-import { deleteData } from "../../api";
+import { deleteRecipe } from "../../api";
 import { recipeModel } from "../../models";
 import "./RecipeCard.css";
 
 type recipeCard = recipeModel & { getData: () => void };
-const RecipeCard = ({
-    _id,
-    title,
-    time,
-    ingredients,
-    instructions,
-    image,
-    description,
-    getData,
-}: recipeCard) => {
-    let navigate = useNavigate();
+const RecipeCard = ({ _id, title, time, ingredients, instructions, image, description, getData }: recipeCard) => {
+  let navigate = useNavigate();
 
-    const deleteRecipe = async () => {
-        const link = `http://localhost:5000/recipes/${_id}`;
-        console.log(link);
+  const deleteRecipeHandler = async () => {
+    await deleteRecipe(_id);
+    getData();
+  };
 
-        await deleteData(link);
-        getData();
-    };
+  const navigateEditRecipe = () => {
+    navigate(`/edit-recipe/${_id}`);
+  };
 
-    const navigateEditRecipe = () => {
-        navigate(`/edit-recipe/${_id}`);
-    };
+  const navigateViewRecipe = () => {
+    navigate(`/view-recipe/${_id}`);
+  };
 
-    const navigateViewRecipe = () => {
-        navigate(`/view-recipe/${_id}`);
-    };
-
-    return (
-        <div className="card">
-            <img src={image} onClick={navigateViewRecipe} />
-            <div className="cardBody">
-                <h2>{title}</h2>
-                <div className="info">
-                    <p className="time">{time} minutes</p>
-                </div>
-                <div className="buttons">
-                    <button className="editBtn" onClick={navigateEditRecipe}>
-                        Edit
-                    </button>
-                    <button className="deleteBtn" onClick={deleteRecipe}>
-                        Delete
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="card">
+      <img src={image} onClick={navigateViewRecipe} />
+      <div className="cardBody">
+        <h2>{title}</h2>
+        <div className="info">
+          <p className="time">{time} minutes</p>
         </div>
-    );
+        <div className="buttons">
+          <button className="editBtn" onClick={navigateEditRecipe}>
+            Edit
+          </button>
+          <button className="deleteBtn" onClick={deleteRecipeHandler}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeCard;
